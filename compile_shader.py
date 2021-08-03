7db370f53303b876c440fff4530cfc967a4a0c56
mobiuschen <mobiuschen@tencent.com>
2021-08-03 10:21:29 +0800

    Update compile shader script

--- compile_shader.py
+++ compile_shader.py
@@ -12,14 +12,40 @@ GLSLC = VULKAN_SDK + "/bin/glslc"
 HLSL_DIR = "./data/shaders/hlsl"
 
 def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "x:", ["proj="])
+    except getopt.GetoptError:
+        help = "Usage: cmd [options]\n" +\
+               "Options:\n" +\
+               "  -x <language>\n" +\
+               "  --proj=<project>"
+        print(help)
+        sys.exit(2)
+
+    language = "hlsl"
+    proj = "none"
+    for opt, arg in opts:
+        if opt == "-x":
+            language = arg
+        elif opt == "--proj":
+            proj = arg
+
     print("GLSLC version:")
     os.system("{glslc} --version".format(glslc=GLSLC))
 
+    print("\n")
+    print("Compile {lang} shaders in project {dir}".format(lang=language, dir=proj))
+    
     dir = os.walk(HLSL_DIR)
     for path, dirs, files in os.walk(HLSL_DIR):
         # print("files: {files}".format(files=files))
         # print("dirs: {dirs}".format(dirs=dirs))
         # print("path {path}".format(path=path))
+        
+        pathTail = os.path.split(path)[1]
+        if proj != "all" and proj != pathTail:
+            continue;
+
         for file in files:
             _, ext = os.path.splitext(file)
             if ext == ".frag" or ext == ".vert":
@@ -27,6 +53,8 @@ def main(argv):
                 command = "{glslc} -x hlsl {path}/{file} -o {path}/{file}.spv".format(glslc=GLSLC, file=file, path=path)
                 print(command)
                 os.system(command)
+
+    print("Compiling has done.")
 
 if __name__ == "__main__":
     main(sys.argv[1:]) 
